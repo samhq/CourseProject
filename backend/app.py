3bff912e136147b9313c802c6fd66414f975a19e
Samiul Hoque <samiul@monarko.com>
2021-11-15 04:13:39 +0600

    Added some boilerplate for user scripts and add applicatio apis

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,56 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from bm25_search import search_query
+from search import search_query
+from user import *
+from contents import indexPage
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 CORS(app, origins='*')
 
+app_version = "v1.0"
 
 @app.route('/')
 def index_page():
-    return app.send_static_file('index.html')
+    resp = {}
+    resp["error"] = False
+    resp["message"] = ""
+    resp["version"] = app_version
+    
+    return jsonify(resp)
 
 
-@app.route('/search_query', methods=['POST'])
-def get_query_search_results():
+@app.route('/search', methods=['POST'])
+def get_search_results():
     resp = {}
     if request.method == 'POST' and request.json != None:
         query = request.json.get("query_string")
         username = request.json.get("username")
+        code = request.json.get("code")
         top_n = request.json.get("top_n")
-        resp = search_query(query, username, top_n)
+        
+        if (validate_user(username, code)):
+            resp = search_query(query, username, top_n)
+        else:
+            resp["error"] = True
+            resp["message"] = "cannot validate user request"
+            
+    return jsonify(resp)
+
+@app.route('/add', methods=['POST'])
+def get_search_results():
+    resp = {}
+    if request.method == 'POST' and request.json != None:
+        bookmark_url = request.json.get("url")
+        username = request.json.get("username")
+        code = request.json.get("code")
+        
+        if (validate_user(username, code)):
+            resp = indexPage(bookmark_url, get_user(username, code))
+        else:
+            resp["error"] = True
+            resp["message"] = "cannot validate user request"
+            
     return jsonify(resp)
 
 
---
+++ backend/user.py
@@ -0,0 +1,11 @@
+def register_user():
+    pass
+
+def authenticate_user():
+    pass
+
+def validate_user():
+    pass
+
+def get_user():
+    pass
