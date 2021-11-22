9bb11dd34ba910756ba6294ef05ad9955373fd3b
Xavier Xie <xieyuheng222@hotmail.com>
2021-11-23 00:01:19 +0800

    Add snackbar for user login

--- frontend/personalized-bookmarking-app/src/popup/App.vue
+++ frontend/personalized-bookmarking-app/src/popup/App.vue
@@ -1,32 +1,37 @@
 <template>
-    <v-container id="login">
-        <v-row>
-          <v-col cols="12">
-          <v-toolbar>
-            <v-spacer />
-            <v-toolbar-title class="headline text-uppercase">
-              <h4>Welcome to PBSE! Please login.</h4>
-            </v-toolbar-title>
-            <v-spacer />
-          </v-toolbar>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="1"></v-col>
-          <v-col cols="10"><v-text-field type="text" name="username" v-model="email" placeholder="Email"></v-text-field></v-col>
-          <v-col cols="1"></v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="1"></v-col>
-          <v-col cols="10"><v-text-field type="password" name="password" v-model="password" placeholder="Password"></v-text-field></v-col>
-          <v-col cols="1"></v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="4"></v-col>
-          <v-col cols="4"><v-btn type="button" v-on:click="login()">Login</v-btn></v-col>
-          <v-col cols="4"></v-col>
-        </v-row>
-    </v-container>
+  <v-container id="login">
+    <v-row>
+      <v-col cols="12">
+      <v-toolbar>
+        <v-spacer />
+        <v-toolbar-title class="headline text-uppercase">
+          <h4>Welcome to PBSE! Please login.</h4>
+        </v-toolbar-title>
+        <v-spacer />
+      </v-toolbar>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="1"></v-col>
+      <v-col cols="10"><v-text-field type="text" name="username" v-model="email" placeholder="Email"></v-text-field></v-col>
+      <v-col cols="1"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="1"></v-col>
+      <v-col cols="10"><v-text-field type="password" name="password" v-model="password" placeholder="Password"></v-text-field></v-col>
+      <v-col cols="1"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="4"></v-col>
+      <v-col cols="4"><v-btn type="button" v-on:click="login()">Login</v-btn></v-col>
+      <v-col cols="4"></v-col>
+    </v-row>
+    <v-snackbar v-model="snackbar"> {{ snackbar_text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
@@ -37,25 +42,31 @@ export default {
       console.log(response);
       if (response.status == 'success') {
         // redirect to user's content page
-      } else {
-        // stay on current login page
       }
     });
   },
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      snackbar: false,
+      snackbar_text: "",
     }
   },
   methods: {
     login() {
       chrome.runtime.sendMessage({command: "login", data: {email: this.email, password: this.password}}, (response) => {
         console.log(response);
-        if (response.status == 'success') {
-          // redirect to user's content page
+        if (response) {
+          if (response.status == 'success') {
+            // redirect to user's content page
+          } else {
+            this.snackbar_text = "Error logging in! Please try again.";
+            this.snackbar = true;
+          }
         } else {
-          // handle errors
+          this.snackbar_text = "Error logging in! Please try again.";
+          this.snackbar = true;
         }
       });
     },
