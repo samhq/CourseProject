b813be084a4d17c4a0d8b5b0ef520283e36349ea
Xavier Xie <xieyuheng222@hotmail.com>
2021-11-24 03:06:06 +0800

    Add UI components

--- frontend/personalized-bookmarking-app/src/popup/App.vue
+++ frontend/personalized-bookmarking-app/src/popup/App.vue
@@ -2,14 +2,14 @@
   <v-container id="app">
     <v-container v-if="authed == false" id="login">
       <v-row>
-        <v-col cols="12">
-        <v-toolbar>
-          <v-spacer />
-          <v-toolbar-title class="headline text-uppercase">
-            <h4>Welcome to PBSE! Please login.</h4>
-          </v-toolbar-title>
-          <v-spacer />
-        </v-toolbar>
+        <v-col class="d-flex align-center" cols="12">
+          <v-toolbar>
+            <v-spacer />
+            <v-toolbar-title class="headline text-uppercase">
+              <h4>Welcome to PBSE! Please login.</h4>
+            </v-toolbar-title>
+            <v-spacer />
+          </v-toolbar>
         </v-col>
       </v-row>
       <v-row>
@@ -17,7 +17,9 @@
       </v-row>
       <v-row>
         <v-col cols="1"></v-col>
-        <v-col cols="10"><v-text-field type="text" name="username" v-model="email" placeholder="Email"></v-text-field></v-col>
+        <v-col cols="10">
+          <v-text-field type="text" name="email" v-model="email" placeholder="Email"></v-text-field>
+        </v-col>
         <v-col cols="1"></v-col>
       </v-row>
       <v-row>
@@ -25,7 +27,9 @@
       </v-row>
       <v-row>
         <v-col cols="1"></v-col>
-        <v-col cols="10"><v-text-field type="password" name="password" v-model="password" placeholder="Password"></v-text-field></v-col>
+        <v-col cols="10">
+          <v-text-field type="password" name="password" v-model="password" placeholder="Password"></v-text-field>
+        </v-col>
         <v-col cols="1"></v-col>
       </v-row>
       <v-row>
@@ -33,34 +37,78 @@
       </v-row>
       <v-row>
         <v-col cols="4"></v-col>
-        <v-col class="d-flex align-center" cols="4"><v-btn type="button" v-on:click="login()">Login</v-btn></v-col>
+        <v-col cols="4">
+          <v-btn type="button" v-on:click="login()">Login</v-btn>
+        </v-col>
         <v-col cols="4"></v-col>
       </v-row>
     </v-container>
     <v-container v-else id="content">
       <vue-tabs>
         <v-tab title="Account">
-          <v-toolbar>
-            <v-spacer />
-            <v-toolbar-title class="headline text-uppercase">
-              <h4>Hello, {{ user.email }}!</h4>
-            </v-toolbar-title>
-            <v-spacer />
-          </v-toolbar>
           <v-row>
             <v-col cols="12">
           </v-row>
           <v-row>
+            <v-col cols="12">
+              <v-toolbar>
+                <v-spacer />
+                <v-toolbar-title class="headline text-uppercase">
+                  <h4>Hello, {{ user.email }}!</h4>
+                </v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+            <v-col>
+          </v-row>
+          <v-row>
             <v-col cols="4"></v-col>
-            <v-col class="d-flex align-center" cols="4"><v-btn type="button" v-on:click="logout()">Logout</v-btn></v-col>
+            <v-col cols="4">
+              <v-btn type="button" v-on:click="logout()">Logout</v-btn>
+            <v-col>
             <v-col cols="4"></v-col>
           </v-row>
         </v-tab>
         <v-tab title="New Bookmark">
+          <v-row>
+            <v-col cols="12">
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field type="text" name="bookmark_name" v-model="bookmark_name" placeholder="Bookmark Name"></v-text-field>
+            </v-col>
+          <v-row>
+          <v-row>
+            <v-col cols="4"></v-col>
+            <v-col cols="4">
+              <v-btn type="button" v-on:click="addBookmark()">Add</v-btn>
+            </v-col>
+            <v-col cols="4"></v-col>
+          <v-row>
         </v-tab>
         <v-tab title="Search">
+          <v-row>
+            <v-col cols="12">
+          </v-row>
+          <v-row>
+            <v-col cols="8">
+              <v-text-field type="text" name="query" v-model="query" placeholder="Query"></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-select v-model="selected_top" :items="top_items" item-text="name" item-value="value" persistent-hint return-object single-line></v-select>
+            </v-col>
+          <v-row>
+          <v-row>
+            <v-col cols="4"></v-col>
+            <v-col cols="4">
+              <v-btn type="button" v-on:click="searchBookmarks()">Search</v-btn>
+            </v-col>
+            <v-col cols="4"></v-col>
+          </v-row>
         </v-tab>
         <v-tab title="All Bookmarks">
+          <v-row>
+            <v-col cols="12">
+          </v-row>
         </v-tab>
       </vue-tabs>
     </v-container>
@@ -78,6 +126,11 @@ import 'vue-nav-tabs/themes/vue-tabs.css'
 export default {
   components: {VueTabs, VTab},
   created: function() {
+    // set current tab info
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      this.bookmark_name = tabs[0].title;
+      this.bookmark_url = tabs[0].url;
+    });
     // check if user is logged in
     chrome.runtime.sendMessage({command: "checkAuth"}, (response) => {
       console.log(response);
@@ -98,6 +151,16 @@ export default {
       password: "",
       user: null,
       authed: false,
+      bookmark_name: "",
+      bookmark_url: "",
+      query: "",
+      selected_top: {name: 'Top5', value: 5},
+      top_items: [
+        {name: 'Top5', value: 5},
+        {name: 'Top10', value: 10},
+        {name: 'Top15', value: 15},
+        {name: 'Top20', value: 20},
+      ],
       snackbar: false,
       snackbar_text: "",
     }
@@ -143,7 +206,9 @@ export default {
 </script>
 
 <style scoped>
+
 p {
   font-size: 20px;
 }
+
 </style>
