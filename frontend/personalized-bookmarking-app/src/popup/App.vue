f556d482656983987262c99e0509fb2913780191
Xavier Xie <xieyuheng222@hotmail.com>
2021-11-25 00:24:26 +0800

    Add registration

--- frontend/personalized-bookmarking-app/src/popup/App.vue
+++ frontend/personalized-bookmarking-app/src/popup/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container id="app">
+  <v-app id="app">
     <v-container v-if="authed == false" id="login">
       <v-row>
         <v-col class="d-flex align-center" cols="12">
           <v-toolbar>
             <v-spacer />
             <v-toolbar-title class="headline text-uppercase">
-              <h4>Welcome to PBSE! Please login.</h4>
+              <h5>Welcome to PBSE! Please login.</h5>
             </v-toolbar-title>
             <v-spacer />
           </v-toolbar>
@@ -36,16 +36,20 @@
         <v-col cols="12">
       </v-row>
       <v-row>
-        <v-col cols="4"></v-col>
-        <v-col cols="4">
+        <v-col cols="2"></v-col>
+        <v-col cols="3">
           <v-btn type="button" v-on:click="login()">Login</v-btn>
         </v-col>
-        <v-col cols="4"></v-col>
+        <v-col cols="1"></v-col>
+        <v-col cols="4">
+          <v-btn type="button" v-on:click="register()">Register</v-btn>
+        </v-col>
+        <v-col cols="2"></v-col>
       </v-row>
     </v-container>
     <v-container v-else id="content">
-      <vue-tabs>
-        <v-tab title="Account">
+      <vue-tabs @tab-change="handleTabChange">
+        <v-tab title="Welcome">
           <v-row>
             <v-col cols="12">
           </v-row>
@@ -54,23 +58,11 @@
               <v-toolbar>
                 <v-spacer />
                 <v-toolbar-title class="headline text-uppercase">
-                  <h4>Hello, {{ user.email }}!</h4>
+                  <h6>Hello, {{ user.email }}</h6>
                 </v-toolbar-title>
                 <v-spacer />
               </v-toolbar>
             <v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="4"></v-col>
-            <v-col cols="4">
-              <v-btn type="button" v-on:click="logout()">Logout</v-btn>
-            <v-col>
-            <v-col cols="4"></v-col>
-          </v-row>
-        </v-tab>
-        <v-tab title="New Bookmark">
-          <v-row>
-            <v-col cols="12">
           </v-row>
           <v-row>
             <v-col cols="12">
@@ -78,11 +70,16 @@
             </v-col>
           <v-row>
           <v-row>
-            <v-col cols="4"></v-col>
+            <v-col cols="2"></v-col>
             <v-col cols="4">
-              <v-btn type="button" v-on:click="addBookmark()">Add</v-btn>
+              <v-btn type="button" v-on:click="addBookmark()">Add Bookmark</v-btn>
             </v-col>
-            <v-col cols="4"></v-col>
+            <v-col cols="1">
+            </v-col>
+            <v-col cols="3">
+              <v-btn type="button" v-on:click="logout()">Logout</v-btn>
+            <v-col>
+            <v-col cols="2"></v-col>
           <v-row>
         </v-tab>
         <v-tab title="Search">
@@ -90,11 +87,11 @@
             <v-col cols="12">
           </v-row>
           <v-row>
-            <v-col cols="8">
+            <v-col cols="9">
               <v-text-field type="text" name="query" v-model="query" placeholder="Query"></v-text-field>
             </v-col>
-            <v-col cols="4">
-              <v-select v-model="selected_top" :items="top_items" item-text="name" item-value="value" persistent-hint return-object single-line></v-select>
+            <v-col cols="3">
+              <v-select :items="top_items" item-text="name" item-value="value" v-model="selected_top"></v-select>
             </v-col>
           <v-row>
           <v-row>
@@ -117,31 +114,26 @@
         <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
       </template>
     </v-snackbar>
-  </v-container>
+  </v-app>
 </template>
 
 <script>
-import {VueTabs, VTab} from 'vue-nav-tabs/dist/vue-tabs.js'
-import 'vue-nav-tabs/themes/vue-tabs.css'
+import {VueTabs, VTab} from "vue-nav-tabs/dist/vue-tabs.js"
+import "vue-nav-tabs/themes/vue-tabs.css"
 export default {
   components: {VueTabs, VTab},
   created: function() {
-    // set current tab info
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      this.bookmark_name = tabs[0].title;
-      this.bookmark_url = tabs[0].url;
-    });
+    var vm = this;
+    // set current page title as bookmark name
+    vm.setTitleAsBookmarkName();
     // check if user is logged in
     chrome.runtime.sendMessage({command: "checkAuth"}, (response) => {
-      console.log(response);
-      if (response.status == 'success') {
-        console.log("User is still logged in");
-        this.user = response.message;
-        this.authed = true;
+      if (response.status == "success") {
+        vm.user = response.message;
+        vm.authed = true;
       } else {
-        console.log("User is logged out");
-        this.user = null;
-        this.authed = false;
+        vm.user = null;
+        vm.authed = false;
       }
     });
   },
@@ -154,51 +146,100 @@ export default {
       bookmark_name: "",
       bookmark_url: "",
       query: "",
-      selected_top: {name: 'Top5', value: 5},
+      selected_top: {name: "Top5", value: 5},
       top_items: [
-        {name: 'Top5', value: 5},
-        {name: 'Top10', value: 10},
-        {name: 'Top15', value: 15},
-        {name: 'Top20', value: 20},
+        {name: "Top5", value: 5},
+        {name: "Top10", value: 10},
+        {name: "Top15", value: 15},
+        {name: "Top20", value: 20},
       ],
       snackbar: false,
       snackbar_text: "",
     }
   },
-  methods: {
-    login() {
-      chrome.runtime.sendMessage({command: "login", data: {email: this.email, password: this.password}}, (response) => {
+  methods: {    
+    register() {
+      var vm = this;
+      chrome.runtime.sendMessage({command: "register", data: {email: vm.email, password: vm.password}}, (response) => {
         if (response) {
-          if (response.status == 'success') {
-            console.log("Login Success");
-            this.user = response.message;
-            this.authed = true;
-            console.log(this.user);
+          if (response.status == "success") {
+            vm.user = response.message;
+            vm.authed = true;
+            vm.snackbar_text = "Successfully registered and logged in.";
+            vm.snackbar = true;
+            console.log(vm.user);
           } else {
-            this.snackbar_text = "Error logging in! Please try again.";
-            this.snackbar = true;
+            if (response.message.code == "auth/weak-password"){
+              vm.snackbar_text = "The password provided is too weak.";
+            } else if (response.message.code == "auth/email-already-in-use") {
+              vm.snackbar_text = "The account already exists for that email.";
+            } else {
+              vm.snackbar_text = "Error in registration, please try again later.";
+            }
+            vm.snackbar = true;
           }
         } else {
-          this.snackbar_text = "Error logging in! Please try again.";
-          this.snackbar = true;
+          vm.snackbar_text = "Error in registration, please try again later.";
+          vm.snackbar = true;
+        }
+      });
+    },
+    login() {
+      var vm = this;
+      chrome.runtime.sendMessage({command: "login", data: {email: vm.email, password: vm.password}}, (response) => {
+        if (response) {
+          if (response.status == "success") {
+            vm.user = response.message;
+            vm.authed = true;
+            vm.snackbar_text = "Successfully logged in.";
+            vm.snackbar = true;
+            console.log(vm.user);
+          } else {
+            if (response.message.code == "auth/user-not-found"){
+              vm.snackbar_text = "No user found for that email.";
+            } else if (response.message.code == "auth/wrong-password") {
+              vm.snackbar_text = "Wrong password provided for that user.";
+            } else {
+              vm.snackbar_text = "Error logging in, please try again later.";
+            }
+            vm.snackbar = true;
+          }
+        } else {
+          vm.snackbar_text = "Error logging in, please try again later.";
+          vm.snackbar = true;
         }
       });
     },
     logout() {
+      var vm = this;
       chrome.runtime.sendMessage({command: "logout"}, (response) => {
         if (response) {
-          if (response.status == 'success') {
-            console.log("Logout Success")
-            this.user = null;
-            this.authed = false;
+          if (response.status == "success") {
+            vm.user = null;
+            vm.authed = false;
           } else {
-            this.snackbar_text = "Error logging out! Please try again.";
-            this.snackbar = true;
+            vm.snackbar_text = "Error logging out! Please try again.";
+            vm.snackbar = true;
           }
         } else {
-          this.snackbar_text = "Error logging out! Please try again.";
-          this.snackbar = true;
+          vm.snackbar_text = "Error logging out! Please try again.";
+          vm.snackbar = true;
         }
+      });
+    },
+    handleTabChange(tabIndex, newTab, oldTab){
+      if (newTab.tabId == "Welcome") {
+        this.setTitleAsBookmarkName();
+      }
+      if (newTab.tabId == "All Bookmarks") {
+        // retrieve bookmarks from API and update all_bookmarks variable
+      }
+    },
+    setTitleAsBookmarkName(){
+      var vm = this;
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        vm.bookmark_name = tabs[0].title;
+        vm.bookmark_url = tabs[0].url;
       });
     }
   }
