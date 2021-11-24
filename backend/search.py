fdcbd79342e1188a1ce48edc0b086b2a0a503468
muhuiyu <muhuiyu.grace@gmail.com>
2021-11-24 17:09:14 +0800

    search_query bookmark filter updated

--- backend/search.py
+++ backend/search.py
@@ -10,34 +10,34 @@ class IndexBM25Okapi(BM25Okapi):
         # print(query, scores)
         top_n = np.argsort(scores)[::-1][:n]
         return top_n
-        # return [(i, documents[i]) for i in top_n]
 
-def search_query(query_terms, user_id, top_n, use_keyword_filter=True):
+def search_query(query, user_id, top_n, use_keyword_filter=True):
+    tokenized_query = preprocess_document_or_query(query)
+
     if use_keyword_filter:
-        filtered_urls = fetch_bookmarks(user_id, fetch_candidates_docids(user_id, query_terms))
-        bookmark_contents = [(preprocess_document_or_query(crawl(url))) for url in filtered_urls ]
+        query_terms = tokenized_query.split()
+        bookmark_contents = [ fetch_contents(user_id)[i] for i in fetch_candidates_docids(user_id, query_terms) ]
     else:
         bookmark_contents = fetch_contents(user_id)
 
     BM25Okapi = IndexBM25Okapi
     bm25 = BM25Okapi(bookmark_contents)
-    tokenized_query = remove_stop_words_and_tokenize(remove_special_characters(' '.join(query_terms)))
     top_results = bm25.get_top_n_with_indice(tokenized_query, bookmark_contents, n=top_n)
-    print(top_results)
+    # print(top_results)
 
     return fetch_bookmarks(user_id, top_results)
 
 # if __name__ == '__main__':
-    # user_id = "id_1"
-    # msg1 = indexPage("https://realpython.com/beautiful-soup-web-scraper-python/", user_id)
-    # msg2 = indexPage("https://stackoverflow.com/questions/4690600/python-exception-message-capturing", user_id)
-    # msg3 = indexPage("http://theory.stanford.edu/~amitp/GameProgramming/AStarComparison.html#the-a-star-algorithm", user_id)
-    # msg4 = indexPage("https://dictionarylist.com/en-ur/traceback/", user_id)
-    # msg5 = indexPage("https://open.cs.uwaterloo.ca/python-from-scratch/7/7/transcript", user_id)
-    # msg6 = indexPage("https://renkun.me/2020/03/31/a-simple-way-to-show-stack-trace-on-error-in-r/", user_id)
-    # query_terms = ['tracebackdef', 'blog']
-    #
-    # n = 2
-    # use_keyword_filter = True
-    # top_n = search_query(query_terms, user_id, n, use_keyword_filter)
-    # print(top_n)
+#     user_id = "id_1"
+#     msg1 = indexPage("https://realpython.com/beautiful-soup-web-scraper-python/", user_id)
+#     msg2 = indexPage("https://stackoverflow.com/questions/4690600/python-exception-message-capturing", user_id)
+#     msg3 = indexPage("http://theory.stanford.edu/~amitp/GameProgramming/AStarComparison.html#the-a-star-algorithm", user_id)
+#     msg4 = indexPage("https://dictionarylist.com/en-ur/traceback/", user_id)
+#     msg5 = indexPage("https://open.cs.uwaterloo.ca/python-from-scratch/7/7/transcript", user_id)
+#     msg6 = indexPage("https://renkun.me/2020/03/31/a-simple-way-to-show-stack-trace-on-error-in-r/", user_id)
+#     query = 'tracebackdef blog'
+#
+#     n = 2
+#     use_keyword_filter = True
+#     top_n = search_query(query, user_id, n, use_keyword_filter)
+#     print(top_n)
