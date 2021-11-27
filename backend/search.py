6ad04efd8ed2fcb81ea0da9e547a37493381b30b
Samiul Hoque <samiul@monarko.com>
2021-11-28 05:40:20 +0600

    Finalize the doc structure

--- backend/search.py
+++ backend/search.py
@@ -1,15 +1,12 @@
 from rank_bm25 import BM25Okapi
-from contents import *
-from utilities import *
+from utilities import tokenize_str
 import numpy as np
 import operator
 
 
 def search_query(query, top_n, corpus):
-    tokenized_corpus = [remove_stop_words_and_tokenize(
-        remove_special_characters(doc)) for doc in corpus]
-    tokenized_query = remove_stop_words_and_tokenize(
-        remove_special_characters(query))
+    tokenized_corpus = [doc.split(" ") for doc in corpus]
+    tokenized_query = tokenize_str(query)
     bm25 = BM25Okapi(tokenized_corpus)
     scores = bm25.get_scores(tokenized_query)
     d = dict()
@@ -21,16 +18,3 @@ def search_query(query, top_n, corpus):
     top = dict(cd[:int(top_n)])
 
     return top
-
-# corpus = ["data mining test she her you he is me are analysis data mining text information her me",
-#           "tranformers her she me them it is avengers movie blockbuster pirates of the carribean toy story yes",
-#           "elon musk tesla paypal space electric vehicle her me you the mining"]
-#
-# tokenized_corpus = [remove_stop_words_and_tokenize(remove_special_characters(doc)) for doc in corpus]
-# query = "data mining"
-# tokenized_query = remove_stop_words_and_tokenize(remove_special_characters(query))
-# bm25 = BM25Okapi(tokenized_corpus)
-# doc_scores = bm25.get_scores(tokenized_query)
-# top_1 = bm25.get_top_n(tokenized_query, corpus, n=1)
-# print(doc_scores)
-# print(top_1)
--- backend/utilities.py
+++ backend/utilities.py
@@ -1,19 +1,37 @@
 import re
-from gensim.parsing.preprocessing import remove_stopwords, STOPWORDS
+from gensim.parsing.preprocessing import remove_stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
 
-def remove_special_characters(text):
-    return re.sub('[^a-zA-Z0-9 ]', '', text)
+def remove_special_characters(lst):
+    lst1 = list()
+    for element in lst:
+        strs = re.sub('[^a-zA-Z0-9 ]', '', element)
+        lst1.append(strs)
+    return lst1
 
 
-def remove_stop_words_and_tokenize(text):
-    return remove_stopwords(text.lower())
+def stemming(lst):
+    lst1 = list()
+    ps = PorterStemmer()
+    for word in lst:
+        lst1.append(ps.stem(word))
+
+    return lst1
+
 
 def tokenize_str(text):
-    # 1. Tokenization
-    # 2. Removing special characters
-    # 3. Removing stop words
-    # 4. Normalization
-    # 5. Stemming
-    pass
+    # 1. Removing stop words
+    text = remove_stopwords(text.lower())
+
+    # 2. Tokenization
+    words = word_tokenize(text)
+
+    # 3. Removing special characters
+    words = remove_special_characters(words)
+
+    # 4. Stemming
+    words = stemming(words)
+
+    return words
